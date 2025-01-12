99570a5a47d4588c191d7a9db7cc799a77612be9
bibimingming <mingming611209@outlook.com>
2025-01-12 14:08:48 +0800

    num.c hello.c 注释

--- hello.c
+++ hello.c
@@ -1,3 +1,4 @@
+//hello world !
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
--- num.c
+++ num.c
@@ -1,13 +1,17 @@
+//本程序是关于变量的，定义变量，赋值，打印赋值后的变量
+//c标准库
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
+  //int定义整数变量
   int dog,cat,cow;
   dog = 1;
   cat = 2;
   cow = 3;
+  //%d用于整数类型
   printf("There is %d dog,%d cats and %d cows\n",dog,cat,cow);
   return 0;
 }
