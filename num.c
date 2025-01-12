0a3af87cba66a85cecf442c295ff36eb8cf60e83
bibimingming <mingming611209@outlook.com>
2025-01-12 13:55:53 +0800

    num.c

---
+++ num.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int dog,cat,cow;
+  dog = 1;
+  cat = 2;
+  cow = 3;
+  printf("There is %d dog,%d cats and %d cows\n",dog,cat,cow);
+  return 0;
+}
